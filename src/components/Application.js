a8d5396af357a72e58cd93eeafa9f3dc9885e6a2
Seun Soetan <seun@strata.ltd>
2021-12-28 21:31:23 -0500

    The selector function-getInterview is added to helpers and included in application.js to get the interview details

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
 import axios from 'axios';
+import { getAppointmentsForDay, getInterview } from 'helpers/selectors';
 
 // const days = [
 //   {
@@ -22,82 +23,170 @@ import axios from 'axios';
 //   },
 // ];
 
-const appointments = [
-  {
-    id: 1,
-    time: '12pm',
-  },
-  {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 1,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
-      },
-    },
-  },
-  {
-    id: 3,
-    time: '2pm',
-    interview: {
-      student: 'Bunni Jones',
-      interviewer: {
-        id: 2,
-        name: 'Tori Malcolm',
-        avatar: 'https://i.imgur.com/Nmx0Qxo.png',
-      },
-    },
-  },
-  {
-    id: 4,
-    time: '3pm',
-    interview: {
-      student: 'Jonni ',
-      interviewer: {
-        id: 3,
-        name: 'Mildred Nazir',
-        avatar: 'https://i.imgur.com/T2WwVfS.png',
-      },
-    },
-  },
-  {
-    id: 5,
-    time: '4pm',
-  },
-  {
-    id: 6,
-    time: '5pm',
-    interview: {
-      student: 'James John',
-      interviewer: {
-        id: 4,
-        name: 'Cohana Roy',
-        avatar: 'https://i.imgur.com/FK8V841.jpg',
-      },
-    },
-  },
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: '12pm',
+//   },
+//   {
+//     id: 2,
+//     time: '1pm',
+//     interview: {
+//       student: 'Lydia Miller-Jones',
+//       interviewer: {
+//         id: 1,
+//         name: 'Sylvia Palmer',
+//         avatar: 'https://i.imgur.com/LpaY82x.png',
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     time: '2pm',
+//     interview: {
+//       student: 'Bunni Jones',
+//       interviewer: {
+//         id: 2,
+//         name: 'Tori Malcolm',
+//         avatar: 'https://i.imgur.com/Nmx0Qxo.png',
+//       },
+//     },
+//   },
+//   {
+//     id: 4,
+//     time: '3pm',
+//     interview: {
+//       student: 'Jonni ',
+//       interviewer: {
+//         id: 3,
+//         name: 'Mildred Nazir',
+//         avatar: 'https://i.imgur.com/T2WwVfS.png',
+//       },
+//     },
+//   },
+//   {
+//     id: 5,
+//     time: '4pm',
+//   },
+//   {
+//     id: 6,
+//     time: '5pm',
+//     interview: {
+//       student: 'James John',
+//       interviewer: {
+//         id: 4,
+//         name: 'Cohana Roy',
+//         avatar: 'https://i.imgur.com/FK8V841.jpg',
+//       },
+//     },
+//   },
+// ];
 
 export default function Application(props) {
   const [state, setState] = useState({
-    day: "Monday",
+    day: 'Monday',
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {}
+    appointments: [
+      {
+        id: 1,
+        time: '12pm',
+      },
+      {
+        id: 2,
+        time: '1pm',
+        interview: {
+          student: 'Lydia Miller-Jones',
+          interviewer: {
+            id: 1,
+            name: 'Sylvia Palmer',
+            avatar: 'https://i.imgur.com/LpaY82x.png',
+          },
+        },
+      },
+      {
+        id: 3,
+        time: '2pm',
+        interview: {
+          student: 'Bunni Jones',
+          interviewer: {
+            id: 2,
+            name: 'Tori Malcolm',
+            avatar: 'https://i.imgur.com/Nmx0Qxo.png',
+          },
+        },
+      },
+      {
+        id: 4,
+        time: '3pm',
+        interview: {
+          student: 'Jonni ',
+          interviewer: {
+            id: 3,
+            name: 'Mildred Nazir',
+            avatar: 'https://i.imgur.com/T2WwVfS.png',
+          },
+        },
+      },
+      {
+        id: 5,
+        time: '4pm',
+      },
+      {
+        id: 6,
+        time: '5pm',
+        interview: {
+          student: 'James John',
+          interviewer: {
+            id: 4,
+            name: 'Cohana Roy',
+            avatar: 'https://i.imgur.com/FK8V841.jpg',
+          },
+        },
+      },
+    ],
+    interviewers: {}
   });
   // const [days, setDays] = useState([]);
   //const [days, setDays] = useState('Monday');
 
   useEffect(() => {
     const daysURL = 'http://localhost:8001/api/days';
-    axios.get(daysURL).then(response =>setDays(response.data));
+    const appointmentsURL = 'http://localhost:8001/api/appointments';
+    const interviewersURL = 'http://localhost:8001/api/interviewers';
+    Promise.all([
+      axios.get(daysURL),
+      axios.get(appointmentsURL),
+      axios.get(interviewersURL),
+    ]).then(all => {
+      // console.log(all[0].data);
+      // console.log(all[1].data);
+      // console.log(all[2].data);
+      const [first, second, third] = all;
+      // console.log(first.data, second.data, third.data);
+      setState(prev => ({
+        ...prev,
+        days: first.data,
+        appointments: second.data,
+        interviewers: third.data,
+      }));
+    });
   }, []);
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -120,7 +209,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
-        {appointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>)}
+        {schedule && schedule}
         <Appointment key="last" time="6pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,21 +1,45 @@
 export function getAppointmentsForDay(state, day) {
-    //... returns an array of appointments for that day
-    let stateCopy = { ...state };
-    if (stateCopy.days.length === 0) {
-        return [];
-    }
-    let result = stateCopy.days.filter(d => d.name === day);
-    if (result.length === 0) {
-        return [];
-    }
-    let appointmentsForDay = result[0].appointments;
-    let appointmentsKeys = Object.keys(stateCopy.appointments);
-    let finalResult=[];
-
-    for (let appID of appointmentsForDay) {
-        if (appointmentsKeys.includes(appID.toString())) {
-            finalResult.push(stateCopy.appointments[appID.toString()]);
-        }
-    }
-    return finalResult;
+  //... returns an array of appointments for that day
+  let stateCopy = { ...state };
+  if (stateCopy.days.length === 0) {
+    return [];
   }
+  let result = stateCopy.days.filter(d => d.name === day);
+  if (result.length === 0) {
+    return [];
+  }
+  let appointmentsForDay = result[0].appointments;
+  let appointmentsKeys = Object.keys(stateCopy.appointments);
+  let finalResult = [];
+
+  for (let appID of appointmentsForDay) {
+    if (appointmentsKeys.includes(appID.toString())) {
+      finalResult.push(stateCopy.appointments[appID.toString()]);
+    }
+  }
+  return finalResult;
+}
+
+export function getInterview(state, interview) {
+  //... returns an interview object for the given interview
+  //... or null if there is no interview
+  let stateCopy = { ...state };
+  let finalResult = {};
+  const appointmentsValues = Object.values(stateCopy.appointments);
+  if (interview === null) {
+    return null;
+  }
+  appointmentsValues.map(appointment => {
+    let interviewersKeys = Object.keys(stateCopy.interviewers);
+    let interviewersId = interview.interviewer;
+    let interviewerDetails = {};
+    if (interviewersKeys.includes(interviewersId.toString())) {
+      interviewerDetails = stateCopy.interviewers[interviewersId.toString()];
+      finalResult = {
+        student: interview.student,
+        interviewer: interviewerDetails,
+      };
+    }
+  });
+  return finalResult;
+}
