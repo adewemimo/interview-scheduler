31e641a344ddd84d0e00ef32f9445c84128d1396
Seun Soetan <seun@strata.ltd>
2022-01-09 20:38:18 -0500

    custom hook - useVisualMode with Test code

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+export function useVisualMode(initial) {
+    const [mode, setMode] = useState(initial);
+    const [history, setHistory] = useState([initial]);
+
+    const transition = (mode, replace=false) => {
+        setMode(mode);
+        if (replace) {
+            setHistory(prev => [...prev.slice(0, prev.length - 1), mode]);
+
+        }
+        else {
+            setHistory(prev => [...prev, mode]);
+        }
+    };
+
+    const back = () => {
+        const historyLength = history.length;
+        if (historyLength > 1) {
+            setHistory(history.slice(0, historyLength - 1));
+            setMode(history[historyLength - 2]);
+            //setMode(history[historyLength - 1]);
+        }
+    }
+
+    return{
+        mode,
+        transition,
+        back
+    };
+}
