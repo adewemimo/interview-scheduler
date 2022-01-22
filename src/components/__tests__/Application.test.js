a1e8f107c8b01e5c6f5ff0d108bdd5735710887e
Seun Soetan <seun@strata.ltd>
2022-01-22 15:16:18 -0500

    use async/await instead

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React from 'react';
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from '@testing-library/react';
 
-import Application from "components/Application";
+import Application from 'components/Application';
 
 afterEach(cleanup);
 
-describe("Application", () => {
-  it('defaults to Monday and changes the schedule when a new day is selected', () => {
-    const { getByText } = render(<Application />);
-  
-    return waitForElement(() => getByText('Monday')).then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });;
-  });
-})
+describe('Application', () => {
+  // it('defaults to Monday and changes the schedule when a new day is selected', () => {
+  //   const { getByText } = render(<Application />);
 
+  //   return waitForElement(() => getByText('Monday')).then(() => {
+  //     fireEvent.click(getByText("Tuesday"));
+  //     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  //   });;
+  // });
+
+  //use async await to test the application
+  it('changes the schedule when a new day is selected', async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText('Monday'));
+
+    fireEvent.click(getByText('Tuesday'));
+
+    expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  });
+});
